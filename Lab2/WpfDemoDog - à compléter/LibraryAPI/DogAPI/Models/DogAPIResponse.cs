bbccddf52b8cece29ec07f8e2c4a8769e22ec44a
SMI\2432978 <2432978@etu.cchic.ca>
2026-02-05 10:33:15 -0500

    Added lab3

---
+++ Lab2/WpfDemoDog - à compléter/LibraryAPI/DogAPI/Models/DogAPIResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.DogAPI.Models
+{
+    public class DogAPIResponse
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        //TODO : Compléter... Ajouter d'autre modèle au besoin...
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+    }
+}
---
+++ Lab2/WpfDemoDog - à compléter/WpfDemoDog/ConfigHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WpfDemoDog
+{
+    class ConfigHelper
+    {
+        public static IConfiguration Config =
+            new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+    }
+}
