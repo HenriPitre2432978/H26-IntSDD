275ed5bfa64fb365041cfdfe88f7ad098134ed9f
ASUS\henri <2432978@etu.cchic.ca>
2026-02-15 15:14:46 -0500

    Readded PartieA

---
+++ Sommatif1/2432978_HPitre_Sommatif1/PartieB/StudentClient/Configs/ConfigHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace StudentClient.Configs
+{
+    public static class ConfigHelper
+    {
+        public static IConfiguration Config =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // SetBasePath nécessite le package Microsoft.Extensions.Configuration.Json
+                .AddJsonFile("Configs/appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+        //Liasion des paramètres de config dans appsettings.json en paramètres "appelables" dans ce projet
+        public static string publicAPIBaseUrl => Config["publicAPIBaseUrl"];
+        public static string localAPIBaseUrl => Config["localAPIBaseUrl"];
+        public static string ApiKey => Config["ApiKey"];
+        public static string Token => Config["Token"];
+    }
+}
